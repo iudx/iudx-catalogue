934f2b2f3702197e212fc91488f0b55f7130b73c
Swaminathan Vasanth Rajaraman <swaminathanvasanth.r@gmail.com>
2019-01-30 13:17:26 +0530

    Adding API, validator & database Verticles

---
+++ src/main/java/iudx/catalogue/database/DatabaseVerticle.java
@@ -0,0 +1,146 @@
+package iudx.catalogue.database;
+
+import java.util.logging.Logger;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonObject;
+
+public class DatabaseVerticle extends AbstractVerticle {
+	
+	private final static Logger logger = Logger.getLogger(DatabaseVerticle.class.getName());
+	String action;
+	JsonObject request_body;
+	
+	@Override
+	public void start(Future<Void> startFuture) {
+
+		logger.info("Database Verticle started!");
+		vertx.eventBus().consumer("database", message -> {
+
+			Future<Void> requestHandler = validateRequest(message);
+
+			if (requestHandler.succeeded()) 
+			{
+				message.reply("success");
+			} else 
+			{
+				message.fail(0, "failure");
+			}
+
+		});
+	}
+
+	private Future<Void> validateRequest(Message<Object> message) {
+		// TODO Auto-generated method stub
+		logger.info("Database Verticle received message.body() = " + message.body());
+		
+		action = (String) message.headers().get("action");
+		request_body = (JsonObject) message.body();
+		
+		Future<Void> messageHandler = Future.future();
+
+		switch (action) 
+		{
+			case "read-item" : 
+			{
+				read_item(messageHandler);
+				break;
+			}
+		
+			case "read-schema" : 
+			{
+				read_schema(messageHandler);
+				break;
+			}
+
+			
+			case "write-item" : 
+			{
+				write_item(messageHandler);
+				break;
+			}
+
+			case "write-schema" : 
+			{
+				write_schema(messageHandler);
+				break;
+			}
+			
+			case "update-item" : 
+			{
+				update_item(messageHandler);
+				break;
+			}
+			
+			case "update-schema" : 
+			{
+				update_schema(messageHandler);
+				break;
+			}
+			
+			case "delete-item" : 
+			{
+				delete_item(messageHandler);
+				break;
+			}
+
+			case "delete-schema" : 
+			{
+				delete_schema(messageHandler);
+				break;
+			}
+		}
+		
+		return messageHandler;
+	}
+
+	private void delete_schema(Future<Void> messageHandler) {
+		// TODO Auto-generated method stub
+		// Implement the block
+		messageHandler.complete();		
+	}
+
+	private void delete_item(Future<Void> messageHandler) {
+		// TODO Auto-generated method stub
+		// Implement the block
+		messageHandler.complete();		
+	}
+
+	private void update_schema(Future<Void> messageHandler) {
+		// TODO Auto-generated method stub
+		// Implement the block
+		messageHandler.complete();		
+	}
+
+	private void update_item(Future<Void> messageHandler) {
+		// TODO Auto-generated method stub
+		// Implement the block
+		messageHandler.complete();		
+	}
+
+	private void write_schema(Future<Void> messageHandler) {
+		// TODO Auto-generated method stub
+		// Implement the block
+		messageHandler.complete();
+	}
+
+	private void write_item(Future<Void> messageHandler) {
+		// TODO Auto-generated method stub
+		// Implement the block
+		messageHandler.complete();
+	}
+
+	private void read_schema(Future<Void> messageHandler) {
+		// TODO Auto-generated method stub
+		// Implement the block
+		messageHandler.complete();
+	}
+
+	private void read_item(Future<Void> messageHandler) {
+		// TODO Auto-generated method stub
+		// Implement the block
+		messageHandler.complete();
+		
+	}
+}
---
+++ src/main/java/iudx/catalogue/validator/ValidatorVerticle.java
@@ -0,0 +1,75 @@
+package iudx.catalogue.validator;
+
+import java.util.logging.Logger;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonObject;
+
+public class ValidatorVerticle extends AbstractVerticle {
+
+	private final static Logger logger = Logger.getLogger(ValidatorVerticle.class.getName());
+	String action;
+	JsonObject request_body;
+	
+	@Override
+	public void start(Future<Void> startFuture) {
+		logger.info("Validator Verticle started!");
+		
+		vertx.eventBus().consumer("validator", message -> {
+
+			Future<Void> requestHandler = validateRequest(message);
+
+			if (requestHandler.succeeded()) 
+			{
+				message.reply("success");
+			} else 
+			{
+				message.fail(0, "failure");
+			}
+
+		});
+	}
+
+	private Future<Void> validateRequest(Message<Object> message) {
+		// TODO Auto-generated method stub
+		logger.info("Validator Verticle received message.body() = " + message.headers());
+
+		action = message.headers().get("action");
+		request_body = (JsonObject) message.body();
+		
+		Future<Void> messageHandler = Future.future();
+
+		switch (action) 
+		{
+			case "validate-item" : 
+			{
+				validate_item(messageHandler);
+				break;
+			}
+		
+			case "validate-schema" : 
+			{
+				validate_schema(messageHandler);
+				break;
+			}
+		}
+		
+		return messageHandler;
+	}
+
+	private void validate_schema(Future<Void> messageHandler) {
+		// TODO Auto-generated method stub
+
+		// Implement the validation item block
+		
+		messageHandler.complete();
+	}
+
+	private void validate_item(Future<Void> messageHandler) {
+		// TODO Auto-generated method stub
+
+		// Implement the validation schema block
+		messageHandler.complete();
+	}
+}
