b75556f128b2c9a86ea9ee53bf410aa761ceb851
adp01 <arundprabhu01@gmail.com>
2019-02-06 16:02:34 +0530

    Formatted the code according to Google checkstyle.

--- src/main/java/iudx/catalogue/validator/ValidatorInterface.java
+++ src/main/java/iudx/catalogue/validator/ValidatorInterface.java
@@ -4,9 +4,8 @@ import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 
 public interface ValidatorInterface {
-	
-	public void validate_item(Future<Void> messageHandler, JsonObject request_body);
-	
-	public void validate_schema(Future<Void> messageHandler, JsonObject request_body);
 
+  public void validate_item(Future<Void> messageHandler, JsonObject item, JsonObject schema);
+
+  public void validate_schema(Future<Void> messageHandler, JsonObject request_body);
 }
