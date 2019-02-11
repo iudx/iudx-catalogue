cce4884eeebd5405af5e3688063fa817f767ba8e
53 68 79 61 6D 61 6C <vaderiashyamal@gmail.com>
2019-02-11 12:20:11 +0530

    Fixed Issue #20 in DatabaseVerticle
    [1] Removed use of messageHandler completely.

--- src/main/java/iudx/catalogue/database/DatabaseInterface.java
+++ src/main/java/iudx/catalogue/database/DatabaseInterface.java
@@ -1,6 +1,5 @@
 package iudx.catalogue.database;
 
-import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonObject;
@@ -9,21 +8,21 @@ public interface DatabaseInterface {
 
   public void init_db(Vertx vertx, JsonObject config);
 
-  public void search_attribute(Future<Void> messageHandler, Message<Object> message);
+  public void search_attribute(Message<Object> message);
 
-  public void read_item(Future<Void> messageHandler, Message<Object> message);
+  public void read_item(Message<Object> message);
 
-  public void read_schema(Future<Void> messageHandler, Message<Object> message);
+  public void read_schema(Message<Object> message);
 
-  public void write_item(Future<Void> messageHandler, Message<Object> message);
+  public void write_item(Message<Object> message);
 
-  public void write_schema(Future<Void> messageHandler, Message<Object> message);
+  public void write_schema(Message<Object> message);
 
-  public void update_item(Future<Void> messageHandler, Message<Object> message);
+  public void update_item(Message<Object> message);
 
-  public void update_schema(Future<Void> messageHandler, Message<Object> message);
+  public void update_schema(Message<Object> message);
 
-  public void delete_item(Future<Void> messageHandler, Message<Object> message);
+  public void delete_item(Message<Object> message);
 
-  public void delete_schema(Future<Void> messageHandler, Message<Object> message);
+  public void delete_schema(Message<Object> message);
 }
