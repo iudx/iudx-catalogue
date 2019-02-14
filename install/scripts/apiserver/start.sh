f29612acaddfcfe46a7b7be6f0d91f3bece33958
Swaminathan Vasanth Rajaraman <swaminathanvasanth.r@gmail.com>
2019-02-14 10:48:06 +0530

    Updating installation scripts

--- install/scripts/apiserver/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar catalogue-apiserver-0.0.1-jar-with-dependencies.jar >> start.out 
--- install/scripts/mongodb/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mongo catalogue --eval 'db.createCollection("schemas"); db.createCollection("items")'
-#mongo admin --eval 'db.createUser( { user: "root", pwd: "root", roles:["root"] } );'
-#mongo catalogue --eval 'db.createUser( { user: "cat-admin", pwd: "cat-admin", roles:["readWrite"] } );'
