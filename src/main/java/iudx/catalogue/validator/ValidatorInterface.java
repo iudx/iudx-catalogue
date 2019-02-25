378c217ba241887d04fad6484fc33d8b8dc342d5
53 68 79 61 6D 61 6C <vaderiashyamal@gmail.com>
2019-02-25 16:37:27 +0530

    Documentation for ValidatorVerticle

--- src/main/java/iudx/catalogue/validator/ValidatorInterface.java
+++ src/main/java/iudx/catalogue/validator/ValidatorInterface.java
@@ -1,8 +1,15 @@
 package iudx.catalogue.validator;
 
 import io.vertx.core.json.JsonObject;
-
+/** Validates an JSON Item against a JSON schema */
 public interface ValidatorInterface {
 
+  /**
+   * Validate item against schema. Throws an Exception when item is invalid
+   *
+   * @param item - the item which will be validated
+   * @param schema - the schema against which the validation will be done
+   * @throws Exception - when validation fails
+   */
   public void validate_item(JsonObject item, JsonObject schema) throws Exception;
 }
