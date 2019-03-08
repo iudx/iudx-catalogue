d2cc21ef2e07415cc1f7ac08b9d31df42b6d5dfa
53 68 79 61 6D 61 6C <vaderiashyamal@gmail.com>
2019-03-08 13:49:30 +0530

    First demo test added.
    [1] The test is failing as of now because of some bug in the application
    [2] Logger was turned off.

--- src/test/java/iudx/catalogue/testcase/ApiTest.java
+++ src/test/java/iudx/catalogue/testcase/ApiTest.java
@@ -1,5 +1,7 @@
 package iudx.catalogue.testcase;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.logging.LogManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +12,8 @@ import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.ext.web.codec.BodyCodec;
+import io.vertx.junit5.Checkpoint;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import iudx.catalogue.apiserver.APIServerVerticle;
@@ -21,117 +25,148 @@ import iudx.catalogue.validator.ValidatorVerticle;
 @ExtendWith(VertxExtension.class)
 class ApiTest implements CatlogueTesting {
 
-  private Vertx vertx;
-  private WebClient webClient;
+  static WebClient webClient;
 
   @BeforeAll
-  void prepare(VertxTestContext testContext) {
-    vertx = Vertx.vertx();
+  public static void prepare(Vertx vertx, VertxTestContext testContext) {
     int procs = Runtime.getRuntime().availableProcessors();
+
+    LogManager.getLogManager().reset();
+    
+    WebClientOptions options =
+        new WebClientOptions()
+            .setTrustAll(true)
+            .setSsl(true)
+            .setDefaultHost("localhost")
+            .setDefaultPort(8455);
+    webClient = WebClient.create(vertx, options);
+
+    Checkpoint val = testContext.checkpoint(3);
 
     JsonObject databaseConf =
         new JsonObject()
-            .put("http.port", 84545)
+            .put("http.port", 8455)
             .put("mongo_host", "localhost")
             .put("mongo_port", 27017);
 
     String database = "mongo";
 
-    DeploymentOptions options = new DeploymentOptions().setConfig(databaseConf);
-    vertx.deployVerticle(new DatabaseVerticle(database), options);
+    DeploymentOptions DeploymentOptions = new DeploymentOptions().setConfig(databaseConf);
+
+    vertx.deployVerticle(
+        new DatabaseVerticle(database),
+        DeploymentOptions,
+        testContext.succeeding(id -> val.flag()));
 
     ValidatorInterface v = new Validator();
     vertx.deployVerticle(
-        new ValidatorVerticle(v), options, testContext.succeeding(id -> testContext.completeNow()));
+        new ValidatorVerticle(v), DeploymentOptions, testContext.succeeding(id -> val.flag()));
 
     vertx.deployVerticle(
         APIServerVerticle.class.getName(),
-        options.setWorker(true).setInstances(procs * 2),
-        testContext.succeeding(id -> testContext.completeNow()));
-
-    webClient =
-        WebClient.create(
-            vertx, new WebClientOptions().setDefaultHost("localhost").setDefaultPort(84545));
+        DeploymentOptions.setWorker(true).setInstances(procs * 2),
+        testContext.succeeding(id -> val.flag()));
   }
 
   @AfterAll
-  void close_up() {
+  public static void close_up(Vertx vertx) {
     vertx.close();
   }
 
   @Test
   @DisplayName("Testing valid item (JSON and auth) POST to catalogue.")
-  public void insertValidItem(VertxTestContext testContext) {}
+  public void insertValidItem(Vertx vertx, VertxTestContext testContext) {
 
-  @Test
-  @DisplayName("Testing in-valid item (JSON) POST to catalogue.")
-  public void insertInvalidItem(VertxTestContext testContext) {}
+    JsonObject data = new JsonObject().put("hello", "OH yeah hello").put("how", "youhu");
+    webClient
+        .postAbs("https://arun:arunrbccps@localhost:8455/cat/items")
+        .ssl(true)
+        .putHeader("Content-Type", "application/json")
+        .putHeader("skip_validation", "true")
+        .as(BodyCodec.string())
+        .sendJsonObject(
+            data,
+            testContext.succeeding(
+                resp -> {
+                  testContext.verify(
+                      () -> {
+                        assertThat(resp.statusCode()).isEqualTo(200);
+                        assertThat(resp.body().isEmpty()).isFalse();
+                        System.out.println(resp.body()); 
+                        testContext.completeNow();
+                      });
+                }));
+ }
 
-  @Test
-  @DisplayName("Testing valid tag search.")
-  public void validTagSearch(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing in-valid tag search.")
-  public void invalidTagSearch(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing valid attribute filter search.")
-  public void validAttributeFilterSearch(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing in-valid attribute filter search.")
-  public void invalidAttributeFilterSearch(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing home page.")
-  public void testHomePage(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing item search.")
-  public void testItemSearch(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing catalogue search with in-valid key.")
-  public void invalidKeySearch(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing catalogue search with empty key.")
-  public void emptyKeySearch(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing catalogue search with empty attribute filters.")
-  public void emptyAttributeFilterSearch(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing catalogue search with white-spaces in attribute filters.")
-  public void whiteSpaceAttributeFilterSearch(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing catalogue search with valid SINGLE TAG.")
-  public void validSingleTagSearch(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing catalogue search with valid MULTIPLE TAG.")
-  public void validMultipleTagSearch(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing catalogue search with valid SINGLE in-valid TAG.")
-  public void invalidSingleTagSearch(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing catalogue search with valid MULTIPLE in-valid TAG.")
-  public void invalidMultipleTagSearch(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing catalogue search with valid single in-case-sensitive TAG.")
-  public void validCaseInsensitiveTagSearch(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing catalogue search with valid ID.")
-  public void validIdSearch(VertxTestContext testContext) {}
-
-  @Test
-  @DisplayName("Testing catalogue search with in-valid ID.")
-  public void invalidIdSearch(VertxTestContext testContext) {}
+//  @Test
+//  @DisplayName("Testing in-valid item (JSON) POST to catalogue.")
+//  public void insertInvalidItem(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing valid tag search.")
+//  public void validTagSearch(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing in-valid tag search.")
+//  public void invalidTagSearch(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing valid attribute filter search.")
+//  public void validAttributeFilterSearch(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing in-valid attribute filter search.")
+//  public void invalidAttributeFilterSearch(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing home page.")
+//  public void testHomePage(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing item search.")
+//  public void testItemSearch(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing catalogue search with in-valid key.")
+//  public void invalidKeySearch(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing catalogue search with empty key.")
+//  public void emptyKeySearch(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing catalogue search with empty attribute filters.")
+//  public void emptyAttributeFilterSearch(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing catalogue search with white-spaces in attribute filters.")
+//  public void whiteSpaceAttributeFilterSearch(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing catalogue search with valid SINGLE TAG.")
+//  public void validSingleTagSearch(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing catalogue search with valid MULTIPLE TAG.")
+//  public void validMultipleTagSearch(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing catalogue search with valid SINGLE in-valid TAG.")
+//  public void invalidSingleTagSearch(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing catalogue search with valid MULTIPLE in-valid TAG.")
+//  public void invalidMultipleTagSearch(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing catalogue search with valid single in-case-sensitive TAG.")
+//  public void validCaseInsensitiveTagSearch(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing catalogue search with valid ID.")
+//  public void validIdSearch(VertxTestContext testContext) {}
+//
+//  @Test
+//  @DisplayName("Testing catalogue search with in-valid ID.")
+//  public void invalidIdSearch(VertxTestContext testContext) {}
 }
