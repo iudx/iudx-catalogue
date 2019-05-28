39016d1282fa8cfc2614ee6d1244ea6ecc8ff6c6
53 68 79 61 6D 61 6C <vaderiashyamal@gmail.com>
2019-05-28 15:38:31 +0530

    config are read from the command line
    [1] Run the jar file with -conf parameter.
    Ex. `java -jar target/catalogue-apiserver-0.0.1-fat.jar -conf src/main/conf/app_conf.json`

--- src/main/java/iudx/catalogue/CatalogueServer.java
+++ src/main/java/iudx/catalogue/CatalogueServer.java
@@ -1,14 +1,9 @@
 package iudx.catalogue;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.util.logging.Logger;
-import org.json.JSONObject;
-import org.json.JSONTokener;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
-import io.vertx.core.json.JsonObject;
 import iudx.catalogue.apiserver.APIServerVerticle;
 import iudx.catalogue.database.DatabaseVerticle;
 import iudx.catalogue.validator.Validator;
@@ -22,8 +17,7 @@ public class CatalogueServer extends AbstractVerticle {
   @Override
   public void start(Future<Void> startFuture) throws Exception {
 
-
-    DeploymentOptions options = getConfigOptions("app_conf.json");
+    DeploymentOptions options = new DeploymentOptions().setConfig(config());
 
     ValidatorInterface v = new Validator();
 
@@ -63,19 +57,5 @@ public class CatalogueServer extends AbstractVerticle {
             startFuture.fail(event.cause());
           }
         });
-  }
-
-  private static DeploymentOptions getConfigOptions(String filepath) {
-
-    DeploymentOptions options = null;
-
-    try (InputStream inputStream = new FileInputStream(filepath)) {
-      JSONObject rawConfig = new JSONObject(new JSONTokener(inputStream));
-      JsonObject config = new JsonObject(rawConfig.toString());
-      options = new DeploymentOptions().setConfig(config);
-    } catch (Exception e) {
-      System.out.println(e);
-    }
-    return options;
   }
 }
