f7ce053baba87517eaf53d68373e4306162f399f
53 68 79 61 6D 61 6C <vaderiashyamal@gmail.com>
2019-05-28 15:22:31 +0530

    CatalogueServer class extends AbstractVerticle

--- src/main/java/iudx/catalogue/CatalogueServer.java
+++ src/main/java/iudx/catalogue/CatalogueServer.java
@@ -5,8 +5,9 @@ import java.io.InputStream;
 import java.util.logging.Logger;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Vertx;
+import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import iudx.catalogue.apiserver.APIServerVerticle;
 import iudx.catalogue.database.DatabaseVerticle;
@@ -14,38 +15,60 @@ import iudx.catalogue.validator.Validator;
 import iudx.catalogue.validator.ValidatorInterface;
 import iudx.catalogue.validator.ValidatorVerticle;
 
-public class CatalogueServer {
+public class CatalogueServer extends AbstractVerticle {
 
   private static final Logger logger = Logger.getLogger(CatalogueServer.class.getName());
 
-  public static void main(String[] args) {
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
 
-    int procs = Runtime.getRuntime().availableProcessors();
-    Vertx vertx = Vertx.vertx();
 
     DeploymentOptions options = getConfigOptions("app_conf.json");
-    String database = "mongo";
-    vertx.deployVerticle(new DatabaseVerticle(database), options);
 
     ValidatorInterface v = new Validator();
-    vertx.deployVerticle(new ValidatorVerticle(v), options);
 
     vertx.deployVerticle(
-        APIServerVerticle.class.getName(),
-        options.setWorker(true).setInstances(procs * 2),
+        new ValidatorVerticle(v),
+        options,
         event -> {
           if (event.succeeded()) {
-            logger.info("IUDX Catalogue Vert.x API Server is started!");
+            String database = "mongo";
+
+            vertx.deployVerticle(
+                new DatabaseVerticle(database),
+                options,
+                event2 -> {
+                  if (event2.succeeded()) {
+                    int procs = Runtime.getRuntime().availableProcessors();
+                    vertx.deployVerticle(
+                        APIServerVerticle.class.getName(),
+                        options.setWorker(true).setInstances(procs * 2),
+                        event3 -> {
+                          if (event3.succeeded()) {
+                            logger.info("IUDX Catalogue Vert.x API Server is started!");
+                            startFuture.complete();
+                          } else {
+                            logger.info("Unable to start API Verticle " + event3.cause());
+                            startFuture.fail(event3.cause());
+                          }
+                        });
+                  } else {
+                    logger.info("Unable to start Database Verticle " + event2.cause());
+                    startFuture.fail(event2.cause());
+                  }
+                });
+
           } else {
-            logger.info("Unable to start IUDX Catalogue Vert.x API Server " + event.cause());
+            logger.info("Unable to start Validator Verticle " + event.cause());
+            startFuture.fail(event.cause());
           }
         });
   }
 
   private static DeploymentOptions getConfigOptions(String filepath) {
-    
+
     DeploymentOptions options = null;
-    
+
     try (InputStream inputStream = new FileInputStream(filepath)) {
       JSONObject rawConfig = new JSONObject(new JSONTokener(inputStream));
       JsonObject config = new JsonObject(rawConfig.toString());
