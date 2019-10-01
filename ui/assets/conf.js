fffaac951e372bce35fa648129298d6fff2bc8e5
Jishnu P <jishnu.jayakumar182@gmail.com>
2019-10-01 12:33:40 +0530

    Add new capabilties to conf (#188)
    * UPDATES : UI with conf parameters to make it generic instead of being pune specific, conf with map default lat lng, auth base URL, footer content
    * ADD conf for generic catalogue setup | ADD ressource server base url to conf, icon attribution and legend customization to conf, font size to list view icon attribution, icon attribution function | INCREASE icon size in map view
    * REMOVE logging of provider list
    * FIX : smart_city_name and  smart_city_url was missing from conf

--- ui/assets/conf.js
+++ ui/assets/conf.js
@@ -1,6 +1,34 @@
-const cat_conf	=	{
-						"smart_city_iudx_logo":"../assets/img/iudx_pscdcl.png",
-						"smart_city_name":"PSCDCL",
-						"smart_city_url":"https://punesmartcity.in/",
-						"resoure_server_base_URL": "https://pune.iudx.org.in/resource-server/pscdcl/v1" 
-					}
+//General catalogue settings
+const cat_conf = {
+	"smart_city_iudx_logo":"../assets/img/iudx_pscdcl.png",
+	"smart_city_name":"PSCDCL",
+	"smart_city_url":"https://punesmartcity.in/",
+	"resoure_server_base_URL": "https://pune.iudx.org.in/resource-server/pscdcl/v1",
+	"auth_base_URL": "https://auth.iudx.org.in/auth/v1",
+	"api_docs_link": "https://apidocs.iudx.org.in",
+	"map_default_view_lat_lng": [18.5204, 73.8567],
+	"map_default_lat_lng_name": "Pune Office",
+	"map_default_zoom": 12
+}
+
+// set legend icons
+// key-name : resource-server-group name | value : icon url
+const legends = {
+    "streetlight-feeder-sree" : "https://image.flaticon.com/icons/svg/1245/1245929.svg",
+    "aqm-bosch-climo": "https://image.flaticon.com/icons/svg/1808/1808701.svg",
+    "flood-sensor": "https://image.flaticon.com/icons/svg/1890/1890123.svg",
+    "wifi-hotspot": "https://image.flaticon.com/icons/svg/254/254046.svg",
+    "ptz-video camera": "https://image.flaticon.com/icons/svg/1111/1111407.svg",
+    "changebhai": "https://image.flaticon.com/icons/svg/252/252025.svg",
+    "crowd-sourced-safetipin": "https://image.flaticon.com/icons/svg/148/148845.svg",
+    "traffic-incidents": "https://image.flaticon.com/icons/svg/1167/1167993.svg",
+    "itms-mobility": "https://image.flaticon.com/icons/svg/2073/2073073.svg"
+}
+
+//set attribution for used icons -> prefer flaticon as it has a wide range of options and icons are appealling
+const icon_attribution = {
+	"author":"freepik",
+	"author_link":"https://www.flaticon.com/authors/freepik",
+	"site":"flaticon.com",
+	"site_link":"https://flaticon.com"
+}
