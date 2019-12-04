cc5bbd26215f3f3ad3165f1e19902d998e4a798e
Jishnu P <jishnu.jayakumar182@gmail.com>
2019-12-04 12:34:46 +0530

    UPDATE rsg names and ADD link for iitm-forecast

--- ui/assets/conf.js
+++ ui/assets/conf.js
@@ -6,7 +6,7 @@ const cat_conf = {
 	"resoure_server_base_URL": "https://pudx.resourceserver.iudx.org.in/resource-server/pscdcl/v1",
 	"auth_base_URL": "https://auth.iudx.org.in/auth/v1",
 	"api_docs_link": "https://apidocs.iudx.org.in",
-        "resource_server_group_head":"urn:iudx-catalogue-pune:" ,
+    "resource_server_group_head":"urn:iudx-catalogue-pune:" ,
 	"provider_head":"urn:iudx-catalogue-pune:" ,
 	"map_default_view_lat_lng": [18.5644, 73.7858],
 	"map_default_lat_lng_name": "PSCDCL Office",
@@ -25,12 +25,13 @@ const legends = {
 	"pudx-resource-server/ptz-video camera": "https://image.flaticon.com/icons/svg/1111/1111407.svg",
 	"pudx-resource-server/crowd-sourced-changebhai": "https://image.flaticon.com/icons/svg/1200/1200848.svg",
 	"pudx-resource-server/changebhai": "https://image.flaticon.com/icons/svg/1200/1200848.svg",
-	"pudx-resource-server/safetipin": "https://image.flaticon.com/icons/svg/541/541384.svg",
+	"pudx.resourceserver.iudx.org.in/safetipin": "https://image.flaticon.com/icons/svg/541/541384.svg",
 	"pudx-resource-server/traffic-incidents": "https://image.flaticon.com/icons/svg/401/401434.svg",
 	"pudx-resource-server/tomtom": "https://image.flaticon.com/icons/svg/401/401434.svg",
 	"pudx-resource-server/itms-mobility": "https://image.flaticon.com/icons/svg/32/32425.svg",
 	"pudx-resource-server/pune-iitm-aqi": "https://image.flaticon.com/icons/svg/1684/1684375.svg",
-	"pudx.resourceserver.iudx.org.in/pune-itms": "https://image.flaticon.com/icons/svg/32/32425.svg"
+	"pudx-resource-server/pune-iitm-forecast": "https://image.flaticon.com/icons/svg/1213/1213587.svg",
+	"pudx-resource-server/pune-itms": "https://image.flaticon.com/icons/svg/32/32425.svg"
 }
 
 //set attribution for used icons -> prefer flaticon as it has a wide range of options and icons are appealling
