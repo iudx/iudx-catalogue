352c85c72923e541935c5fe11b7f3c7707c14202
Jishnu P <jishnu.jayakumar182@gmail.com>
2020-02-27 11:35:04 +0530

    UPDATE with new getconfig ajax call

--- ui/assets/conf.js
+++ ui/assets/conf.js
@@ -1,47 +1,35 @@
-//General catalogue settings
-const cat_conf = {
-	"smart_city_iudx_logo":"../assets/img/iudx_pscdcl.png",
-	"smart_city_name":"PSCDCL",
-	"smart_city_url":"https://punesmartcity.in/",
-	"resoure_server_base_URL": "https://pudx.resourceserver.iudx.org.in/resource-server/pscdcl/v1",
-	"auth_base_URL": "https://auth.iudx.org.in/auth/v1",
-	"api_docs_link": "https://apidocs.iudx.org.in",
-    "resource_server_group_head":"urn:iudx-catalogue-pune:" ,
-	"provider_head":"urn:iudx-catalogue-pune:" ,
-	"map_default_view_lat_lng": [18.5644, 73.7858],
-	"map_default_lat_lng_name": "PSCDCL Office",
-	"map_default_zoom": 12
-}
+var cat_conf = null
+var legends = null
+var icon_attribution = null
 
-/* set legend icons
-   key-name : resource-server-group name, value : icon url
-*/
+function conf_ajax_call(__url){
+	return new Promise((resolve, reject) => {
+        $.ajax({
+            "url": __url,
+            "async": false,
+            "method": 'GET',
+            success: function (data) {
+                resolve(data)
+            },
+            error: function (error) {
+                reject(error)
+            },
+            timeout: 30000 // sets timeout to 30 seconds
+        })
+    })
+}; 
 
-const legends = {
-	"pudx-resource-server/streetlight-feeder-sree" : "https://image.flaticon.com/icons/svg/1245/1245929.svg",
-	"pudx-resource-server/aqm-bosch-climo": "https://image.flaticon.com/icons/svg/1808/1808701.svg",
-	"pudx-resource-server/flood-sensor": "https://image.flaticon.com/icons/svg/1890/1890123.svg",
-	"pudx-resource-server/wifi-hotspot": "https://image.flaticon.com/icons/svg/660/660488.svg",
-	"pudx-resource-server/ptz-video camera": "https://image.flaticon.com/icons/svg/1111/1111407.svg",
-	"pudx-resource-server/crowd-sourced-changebhai": "https://image.flaticon.com/icons/svg/1200/1200848.svg",
-	"pudx-resource-server/changebhai": "https://image.flaticon.com/icons/svg/1200/1200848.svg",
-	"pudx.resourceserver.iudx.org.in/safetipin": "https://image.flaticon.com/icons/svg/541/541384.svg",
-	"pudx-resource-server/traffic-incidents": "https://image.flaticon.com/icons/svg/401/401434.svg",
-	"pudx-resource-server/tomtom": "https://image.flaticon.com/icons/svg/401/401434.svg",
-	"pudx-resource-server/itms-mobility": "https://image.flaticon.com/icons/svg/32/32425.svg",
-	"pudx-resource-server/pune-iitm-aqi": "https://image.flaticon.com/icons/svg/1684/1684375.svg",
-	"pudx-resource-server/pune-iitm-forecast": "https://image.flaticon.com/icons/svg/1213/1213587.svg",
-	"pudx-resource-server/pune-itms": "https://image.flaticon.com/icons/svg/32/32425.svg"
-}
 
-//set attribution for used icons -> prefer flaticon as it has a wide range of options and icons are appealling
-const icon_attribution = {
-	"author": [
-				{"freepik":"https://www.flaticon.com/authors/freepik"}, 
-				{"smashicons":"https://www.flaticon.com/authors/smashicons"}, 
-				{"flat-icons":"https://www.flaticon.com/authors/flat-icons"}, 
-				{"itim2101":"https://www.flaticon.com/authors/itim2101"}
-			],
-	"site":"flaticon.com",
-	"site_link":"https://flaticon.com"
-}
+conf_ajax_call(window.location["href"]+'catalogue/internal_apis/getconfig')
+	.then(data => {
+
+		cat_conf = data[0]['configurations']
+		legends = data[0]['legends']
+		icon_attribution = data[1]['global_configuration']['icon_attribution']
+		// console.log(cat_conf, legends, icon_attribution)
+	    // _alertify("Success!!!", '<pre id="custom_alertbox">' + jsonPrettyHighlightToId(data) + '</pre>')
+	})
+	.catch(error => {
+	    // _alertify("Error!!!", '<pre id="custom_alertbox">: ' + error["statusText"] + '</pre>');
+	    console.log(error)
+	})
